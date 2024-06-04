3648e05dda6d071644ba7c950daba92296834a74
rajeshrishi2002 <rajeshrishi2002@gmail.com>
2024-06-04 16:39:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+
+var e = 864e5,
+  t = 25569,
+  a = 324e5;
+function i(i) {
+  return (i - t) * e - a;
+}
+window.onload = () => {
+  const e = document.querySelector("#masonry"),
+    t = 1e4;
+  function a() {
+    0 == e.classList.contains("active") &&
+      (e.classList.add("active"),
+      setTimeout(() => {
+        e.classList.remove("active"), e.offsetWidth;
+      }, t));
+  }
+  document.addEventListener("scroll", function () {
+    a();
+  }),
+    window.addEventListener("resize", a),
+    (function () {
+      const t = list.reverse();
+
+        t.splice(0, 0, {
+          title:
+            "<p><a target='_blank' href='princess.jpg'/><B>Hey! Princess</B></a>  Happy Birthday</p>"
+        }),
+   
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/book/'/>BOOK</a></b></p><p>She read books as one would breath air,<br>to fill up and leave.<br>Books are uniquely portable magic,<br>and it is a gift that you can open again and again.</p><p><img id='profile_img' src='book.png'></p>"
+        }), 
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/lucky/'/>LUCKY</a></b></p><p>Sometimes you are forget<br> how lucky you are</p><p><img id='profile_img' src='spin.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p>Ennala maraka mudiyatha oruthangalukaaga oru marakamudiyatha gift kuduthu irukenu namburen😇 ,<br> ithula na solli iruka ellamey true thaan and kandipa neenga life la perusa jeipinga appo ungalukaga santhoshapadura first person naanathaan irupen 🥳and naa yeppomey ungala hurt pandra side iruka maaten,naa yeppomey ungala celebrate pandra side thaan,<br> suppose ithu varaikum yethathu hurt panni irunthenna romba sorry,inimey ippadi nadakathu ☹️,neenga yenaku oru promise pannanum yeppomey happy irupenu or else i will make you the happiest🤗</p>"
+        }),
+        t.splice(0, 0, {
+          title:
+            "<p> A Princess is born to Live,Laugh,and Love</p>"
+        }),
+
+        t.splice(0, 0, {
+          title:
+            "<p><b>28/05/2004<b><br>Friday</p>"
+        }),
+
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/heart/'/>CODER</a></b></p><p>The world simply cannot reject anyone or anything that comes from a place of passion.<br> Stay focused on what you love, <br>keep going <br>When you have passion, you cannot fail.</p><p><img id='profile_img' src='code.png'></p>"
+        }),
+          t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='about_aishu.jpg'/>AISHU</a></b></p><p>She is already hurt,mentally and emotionally.<br>But everyday,She walks with a smile,<br>because that's who she is.<br>The girl never stopped smiling</p><p><img id='profile_img' src='dp.jpg'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/kanmani/'>KANMANI</a></b></p><p>you are as strong as a mermaid, <br>although tossed by the waves,<br> but you will never sink.<br>You always try, make efforts and overcome difficulties in life.</p><p><img id='profile_img' src='file.jpg'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/emoji/'/>EMOJI</a></b></p><p>God has<br> a purpose for your pain,<br> a reason for your struggles <br>and a reward for your faithfulness.<br> Don't give up</p><p><img id='profile_img' src='emo.png'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/doraemon/'/>DORAEMON</a></b></p><p>Take a moment to appreciate<br>how awesome you are,<br>Yeah you!<br>-Doraemon</p><p><img id='profile_img' src='doraemon.png'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='thalapathy.png'/>THALAPATHY</a></b></p><p>Whatever the destination is, you should choose the right way to achieve it.<br>Be like water, it can act according to the situation yet nothing can resist it. <br> Have some haters, it will make your life fun.<br>- Thalapathy</p><p><img id='profile_img' src='av.jpg'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/panda-wishes-you/'/>GIFT VENUMA</a></b></p><p>Some gifts are big.<br>Others are small.<br> But the ones that come from the heart are the best gifts of all.</p><p><img id='profile_img' src='em.png'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/gift-box/'/>GIFT FOR YOU</a></b></p><p>You smile when you are happy,<br>I smile,<br>when i see you happy</p><p><img id='profile_img' src='Gdp.jpg'></p>"
+        }), 
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/taylor/'/>MUSIC</a></b></p><p>If you are lucky enough to find something that you love, <br>and you have a shot at being good at it,<br> don't stop,don't put it down.<br>-Taylor swift</p><p><img id='profile_img' src='ml.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/stressbuster/'/>STRESS BUSTER</a></b></p><p>If you feel like losing everything remember trees lose their leaves every year,<br> and they stand tall and wait for better days to come.</p><p><img id='profile_img' src='ha.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/star/'/>STARS</a></b></p><p>Shine like a star<br> A shining star never gets lost in darkness</p><p><img id='profile_img' src='star.png'></p>"
+        }),t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/birthday/'/>BTS</a></b></p><p>When things get hard,stop for a while and look back and see how far you have come.<br>Do not forget how rewarding it is.<br>You are the most beautiful flower, morethan anyone else in this world.<br>-V</p><p><img id='profile_img' src='v.jpg'></p>"
+        }), t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='virat.jpg'/>VIRAT KOHLI</a></b></p><p>Whatever you want to do,<br>do with the full passion,<br>and work really hard towards it.<br>Do not kook anywhere else.<br>-Virat Kohli</p><p><img id='profile_img' src='vk.jpg'></p>"
+        }), 
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/scratch/'/>SCRATCH</a></b></p><p>You're allowed to scream, <br>you're allowed to cry,<br> but NEVER GIVE UP<br> keep fighting for your dreams</p><p><img id='profile_img' src='scratch.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/developer/'/>DEVELOPER</a></b></p><p>Hey Developer,<br>I want to say to you something:<br>My life was going like a boring HTML <br>once you came into my life as CSS,<br>it has changed to a stylized and beautiful one.</p><p><img id='profile_img' src='lap.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/sleep/'/>SLEEP</a></b></p><p>Let her sleep, when she wakes, she will shake the world,<br>she will move the mountains,<br>you are that much strong and you can handle anything in life with a good sleep.</p><p><img id='profile_img' src='sleep.png'></p>"
+        }),
+      
+        
+      
+        t.forEach((t) => {
+          let a = "";
+          var n = document.createElement("li");
+          t.id
+            ? t.title &&
+              (a =
+                "<img src='https://shots.codepen.io/username/pen/" +
+                t.id +
+                "-1280.jpg' /><h2><a target='_blank' href='https://rajeshrishi2002.github.io" +
+              
+                
+                t.id +
+                "'>" +
+                t.title +
+                "</a><span class='date'>" +
+                new Intl.DateTimeFormat("en", {
+                  month: "short",
+                  year: "numeric"
+                }).format(new Date(i(t.date))) +
+                "</span></h2>")
+            : ((a = t.title), n.classList.add("spanning")),
+            (n.innerHTML = "<div>" + a + "</div>"),
+            e.appendChild(n);
+        });
+    })(),
+    a();
+};

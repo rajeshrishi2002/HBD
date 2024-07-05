4a1fdd1a19b5402e21e36dd1313ea99c1b04b377
rajeshrishi2002 <rajeshrishi2002@gmail.com>
2024-07-05 17:32:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,12 @@ window.onload = () => {
    
         t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/book/'/>BOOK</a></b></p><p>She read books as one would breath air,<br>to fill up and leave.<br>Books are uniquely portable magic,<br>and it is a gift that you can open again and again,</p><p><img id='profile_img' src='book.png'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/book/'/>BOOK</a></b></p><p>She read books as one would breath air,<br>to fill up and leave.<br>Books are uniquely portable magic,<br>and it is a gift that you can <br>open again and again,</p><p><img id='profile_img' src='book.png'></p>"
         }), 
         t.splice(3, 0, {
           title:
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/lucky/'/>LUCKY</a></b></p><p>Sometimes you are forget<br> how lucky you are,</p><p><img id='profile_img' src='spin.png'></p>"
         }),
-        
         
         t.splice(3, 0, {
           title:
@@ -43,15 +42,75 @@ window.onload = () => {
         }),
         t.splice(3, 0, {
           title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/smile/'/>DON'T CRY</a></b></p><p>Your tears are precious,<br>don't waste them on anything less<br>save them for moments of joy, and let happiness be the only reason they fall,</p><p><img id='profile_img' src='smile.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/pei/'/>PEI</a></b></p><p>You're my adorable little ghost, haunting my heart with your sweet presence,</p><p><img id='profile_img' src='pei.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/candle/'/>WISH</a></b></p><p>May your heart's desires dance with the stars <br> and every wish you whisper to the stars bloom into reality,</p><p><img id='profile_img' src='wish.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/DND/'/>DISTURB</a></b></p><p>Let your growth silence those who disturb you, not your anger,<br>keep shine brighter,</p><p><img id='profile_img' src='dnd.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/cook/'/>COOK</a></b></p><p> I want to cook all your favorite foods, <br>It would mean a lot to me to see you enjoying the dishes that bring you happiness,</p><p><img id='profile_img' src='cook.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/Game/'/>GAME</a></b></p><p>Life is like a game<br> Sometimes there are challenges and obstacles, but the key is to enjoy the journey and not worry too much about the ups and downs.<br> Remember, I'm here to cheer you on and celebrate your victories,</p><p><img id='profile_img' src='fp.jpg'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/puppy/'/>TIGER</a></b></p><p>I know how much Tiger meant to you,<br>Pets have a unique way of leaving paw prints on our hearts, <br>and Tiger certainly left a big one on yours,If you ever want to talk about Tiger or share memories of him, I'm here for you,</p><p><img id='profile_img' src='puppy.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/locker/'/>LOCKER</a></b></p><p>I'll protect you like a locker,<br> keeping you safe and secure from all that tries to harm you,</p><p><img id='profile_img' src='lock.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/quiz/'/>QUIZ</a></b></p><p>life is like a quiz, <br>Don't worry too much about the challenges that come your way,<br>I believe in you,<br> and I'm here to support you through out,</p><p><img id='profile_img' src='quiz.jpg'></p>"
+        }),
+      
+        t.splice(3, 0, {
+          title:
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/Askme/'/>ASK ME</a></b></p><p>Whether it's about your studies, life, or even just a random thought that pops into your head, I'm here to listen and help in any way I can. Your well-being and success mean a lot to me,</p><p><img id='profile_img' src='ques.png'></p>"
         }),
         t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/All-the-best/'/>SEMESTER</a></b></p> <p>Remember, while it's essential to study and prepare,it's equally important to take care of yourself.<br>Make sure you're getting enough rest and sleep.<br> A well-rested mind performs at its best, and I have no doubt that you're going to do brilliantly in your exams. <br>So, don't stress too much, and allow yourself some quality rest time,</p><p><img id='profile_img' src='exam.png'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/All-the-best/'/>SEMESTER</a></b></p> <p>Remember, while it's essential to study and prepare,it's equally important to take care of yourself.<br>Make sure you're getting enough rest and sleep.<br> I have no doubt that you're going to do brilliantly in your exams. <br>So, don't stress too much, and allow yourself some quality rest time,</p><p><img id='profile_img' src='exam.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/match/'/>MATCH</a></b></p><p> Here's a cute little brain teaser <br>just for you<br> match the pairs and have fun,</p><p><img id='profile_img' src='emojii.jpg'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/owl/'/>OWL</a></b></p><p>Don't let the chatter of shadows <br>dim your light.Ignore those who speak from behind,</p><p><img id='profile_img' src='owl.png'></p>"
+        }),
+        
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/flowers/'/>FLOWERS</a></b></p><p>You are like a flower in the garden of my life,<br>Just as flowers bloom and bring beauty to the world, <br>you bring light and color into my life every day,</p><p><img id='profile_img' src='flowers.png'></p>"
+        }),
+        t.splice(3, 0, {
+          title:
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/burn/'/>BURN YOUR PAST</a></b></p> <p>Burn bright,<br> rise strong,<br> and let your spirit blossom like a phoenix from the ashes, and shine brighter than ever before,</p><p><img id='profile_img' src='fire.png'></p>"
         }),
         t.splice(0, 0, {
           title:
-            "<p> A Princess is born to Live,Laugh,and Love</p>"
+            "<p> A Princess is born to Live,Laugh,and Love </p>"
         }),
 
         t.splice(0, 0, {
@@ -65,10 +124,10 @@ window.onload = () => {
         }),
           t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='about_aishu.jpg'/>AISHU</a></b></p><p>She is already hurt,mentally and emotionally.<br>But everyday,She walks with a smile,<br>because that's who she is.<br>The girl never stopped smiling,</p><p><img id='profile_img' src='dp.jpg'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/foryou/'/>AISHU</a></b></p><p>She is already hurt,mentally and emotionally.<br>But everyday,She walks with a smile,<br>because that's who she is.<br>The girl never stopped smiling,</p><p><img id='profile_img' src='dp.jpg'></p>"
         }), t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/kanmani/'>KANMANI</a></b></p><p>you are as strong as a mermaid, <br>although tossed by the waves,<br> but you will never sink.<br>You always try, make efforts and overcome difficulties in life,</p><p><img id='profile_img' src='file.jpg'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/kanmani/'>KANMANI</a></b></p><p>You are as strong as a mermaid, <br>although tossed by the waves,<br> but you will never sink.<br>You always try,<br> make efforts and overcome difficulties in life,</p><p><img id='profile_img' src='file.jpg'></p>"
         }), t.splice(3, 0, {
           title:
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/emoji/'/>EMOJI</a></b></p><p>God has<br> a purpose for your pain,<br> a reason for your struggles <br>and a reward for your faithfulness.<br> Don't give up,</p><p><img id='profile_img' src='emo.png'></p>"
@@ -77,10 +136,10 @@ window.onload = () => {
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/doraemon/'/>DORAEMON</a></b></p><p>Take a moment to appreciate<br>how awesome you are,<br>Yeah you!,<br>-Doraemon</p><p><img id='profile_img' src='doraemon.png'></p>"
         }), t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='thalapathy.png'/>THALAPATHY</a></b></p><p>Whatever the destination is, you should choose the right way to achieve it.<br>Be like water, it can act according to the situation yet nothing can resist it. <br> Have some haters, it will make your life fun,<br>- Thalapathy</p><p><img id='profile_img' src='av.jpg'></p>"
+            "<p><b><a target='_blank' href='thalapathy.png'/>THALAPATHY</a></b></p><p>Whatever the destination is,<br> you should choose the right way to achieve it.<br>Be like water, <br>it can act according to the situation yet nothing can resist it. Have some haters, it will make your life fun,<br>- Thalapathy</p><p><img id='profile_img' src='av.jpg'></p>"
         }), t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/panda-wishes-you/'/>GIFT VENUMA</a></b></p><p>Some gifts are big.<br>Others are small.<br> But the ones that come from the heart are the best gifts of all,</p><p><img id='profile_img' src='em.png'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/panda-wishes-you/'/>GIFT VENUMA</a></b></p><p>Some gifts are big.<br>Others are small.<br> But<br> the ones that come from the heart are the best gifts of all,</p><p><img id='profile_img' src='em.png'></p>"
         }), t.splice(3, 0, {
           title:
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/gift-box/'/>GIFT FOR YOU</a></b></p><p>You smile when you are happy,<br>I smile,<br>when i see you happy,</p><p><img id='profile_img' src='Gdp.jpg'></p>"
@@ -91,11 +150,11 @@ window.onload = () => {
         }),
         t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/stressbuster/'/>STRESS BUSTER</a></b></p><p>If you feel like losing everything remember trees lose their leaves every year,<br> and they stand tall and wait for better days to come,</p><p><img id='profile_img' src='ha.png'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/stressbuster/'/>STRESS BUSTER</a></b></p><p>If you feel like losing everything<br> remember <br>trees lose their leaves every year,<br> and they stand tall and wait for better days to come,</p><p><img id='profile_img' src='ha.png'></p>"
         }),
         t.splice(3, 0, {
           title:
-            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/star/'/>STARS</a></b></p><p>Shine like a star<br> A shining star never gets lost in darkness,</p><p><img id='profile_img' src='star.png'></p>"
+            "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/star/'/>STARS</a></b></p><p>Shine like a star<br> A shining star never gets lost in darkness,</p><p><img id='profile_img' src='Star.png'></p>"
         }),t.splice(3, 0, {
           title:
             "<p><b><a target='_blank' href='https://rajeshrishi2002.github.io/birthday/'/>BTS</a></b></p><p>When things get hard,stop for a while and look back and see how far you have come.<br>Do not forget how rewarding it is.<br>You are the most beautiful flower, morethan anyone else in this world,<br>-V</p><p><img id='profile_img' src='v.jpg'></p>"
@@ -147,3 +206,4 @@ window.onload = () => {
     })(),
     a();
 };
+
